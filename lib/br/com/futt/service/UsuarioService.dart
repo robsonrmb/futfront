9d3f9d075465c4d972f4367f9a999e2f154862b4
Robson Brito <robson.rmb@gmail.com>
2020-05-10 02:25:30 +0100

    Reestruturação: UsuarioService e UsuarioRest.

--- lib/br/com/futt/service/UsuarioService.dart
+++ lib/br/com/futt/service/UsuarioService.dart
@@ -1,85 +1,52 @@
 import 'package:com/br/com/futt/constantes/ConstantesRest.dart';
 import 'package:com/br/com/futt/model/UsuarioModel.dart';
+import 'package:com/br/com/futt/rest/UsuarioRest.dart';
 import 'package:com/br/com/futt/service/fixo/UsuarioServiceFixo.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 class UsuarioService {
 
   inclui(var cadastroLoginModel, {bool teste}) async {
-
     if (teste == null || teste == false) {
-      http.Response response = await http.post(
-          "${ConstantesRest.URL_USUARIOS}/adiciona",
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-          },
-          body: jsonDecode(cadastroLoginModel)
-      );
+      String url = "${ConstantesRest.URL_USUARIOS}/adiciona";
+      UsuarioRest usuarioRest = UsuarioRest();
+      usuarioRest.processaHttpPost(url, cadastroLoginModel);
 
-      if (response.statusCode != 200) {
-        throw Exception('Failed to load!!!');
-      }
     }else{
       UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
       serviceFixo.inclui(cadastroLoginModel);
     }
   }
 
-  incluiFoto(var usuarioModel, {bool teste}) async {
-
+  incluiFoto(var usuarioModel, {bool teste}) {
     if (teste == null || teste == false) {
-      http.Response response = await http.post(
-          "${ConstantesRest.URL_USUARIOS}/${usuarioModel.id}/foto",
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-          },
-          body: jsonDecode(usuarioModel)
-      );
+      String url = "${ConstantesRest.URL_USUARIOS}/${usuarioModel.id}/foto, usuarioModel";
+      UsuarioRest usuarioRest = UsuarioRest();
+      usuarioRest.processaHttpPost(url, usuarioModel);
 
-      if (response.statusCode != 200) {
-        throw Exception('Failed to load!!!');
-      }
     }else{
       UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
       serviceFixo.incluiFoto(usuarioModel);
     }
   }
 
-  atualiza(var usuarioModel, {bool teste}) async {
-
+  atualiza(var usuarioModel, {bool teste}) {
     if (teste == null || teste == false) {
-      http.Response response = await http.put(
-          "${ConstantesRest.URL_USUARIOS}",
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-          },
-          body: jsonDecode(usuarioModel)
-      );
+      String url = "${ConstantesRest.URL_USUARIOS}";
+      UsuarioRest usuarioRest = UsuarioRest();
+      usuarioRest.processaHttpPut(url, usuarioModel);
 
-      if (response.statusCode != 200) {
-        throw Exception('Failed to load!!!');
-      }
     }else{
       UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
       serviceFixo.atualiza(usuarioModel);
     }
   }
 
-  desativa(String idUsuario, {bool teste}) async {
-
+  desativa(String idUsuario, {bool teste}) {
     if (teste == null || teste == false) {
-      var usuarioModel;
-      http.Response response = await http.put(
-          "${ConstantesRest.URL_USUARIOS}/desativa/${idUsuario}",
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-          }
-      );
+      String url = "${ConstantesRest.URL_USUARIOS}/desativa/${idUsuario}";
+      UsuarioRest usuarioRest = UsuarioRest();
+      usuarioRest.processaHttpPut(url, "");
 
-      if (response.statusCode != 200) {
-        throw Exception('Failed to load!!!');
-      }
     }else{
       UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
       serviceFixo.desativa(idUsuario);
@@ -87,196 +54,63 @@ class UsuarioService {
   }
 
   exclui(String idUsuario, {bool teste}) async {
-
     if (teste == null || teste == false) {
-      var usuarioModel;
-      http.Response response = await http.delete(
-          "${ConstantesRest.URL_USUARIOS}/${idUsuario}",
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-          }
-      );
+      String url = "${ConstantesRest.URL_USUARIOS}/${idUsuario}";
+      UsuarioRest usuarioRest = UsuarioRest();
+      usuarioRest.processaHttpDelete(url);
 
-      if (response.statusCode != 200) {
-        throw Exception('Failed to load!!!');
-      }
     }else{
       UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
       serviceFixo.exclui(idUsuario);
     }
   }
 
-  Future<UsuarioModel> buscaPorId(String idUsuario, {bool teste}) async {
-
-    http.Response response = await http.get("${ConstantesRest.URL_USUARIOS}/${idUsuario}");
-    if (response.statusCode == 200 || (teste != null && teste == true)) {
-      var dadosJson = json.decode(response.body);
-      if (teste != null && teste == true) {
-        UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
-        dadosJson = serviceFixo.responseObjeto();
-      }
-      return UsuarioModel.fromJson(dadosJson); //.converteJson
-
-    }else{
-      throw Exception('Failed to load Tipo Usuario!!!');
-    }
+  Future<UsuarioModel> buscaPorId(String idUsuario, {bool teste}) {
+    String url = "${ConstantesRest.URL_USUARIOS}/${idUsuario}";
+    UsuarioRest usuarioRest = UsuarioRest();
+    return usuarioRest.processaHttpGetObject(url, teste);
   }
 
   Future<List<UsuarioModel>> listaTodos({bool teste}) async {
-
-    http.Response response = await http.get("${ConstantesRest.URL_USUARIOS}");
-    if (response.statusCode == 200 || (teste != null && teste == true)) {
-      var dadosJson = json.decode(response.body);
-      if (teste != null && teste == true) {
-        UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
-        dadosJson = serviceFixo.responseLista();
-      }
-      List<UsuarioModel> lista = List();
-      for (var registro in dadosJson) {
-        UsuarioModel usuarioModel = UsuarioModel.fromJson(
-            registro); //.converteJson
-        lista.add(usuarioModel);
-      }
-      return lista;
-
-    } else {
-      throw Exception('Failed to load Tipo Torneio!!!');
-    }
+    String url = "${ConstantesRest.URL_USUARIOS}";
+    UsuarioRest usuarioRest = UsuarioRest();
+    return usuarioRest.processaHttpGetList(url, teste);
   }
 
   Future<List<UsuarioModel>> listaPorFiltros(var usuarioModel, {bool teste}) async {
-
-    http.Response response = await http.post(
-        "${ConstantesRest.URL_USUARIOS}/filter",
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: {
-          jsonDecode(usuarioModel)
-        }
-    );
-    var dadosJson = json.decode(response.body);
-
-    if (teste != null && teste == true) {
-      UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
-      dadosJson = serviceFixo.responseLista();
-    }
-    List<UsuarioModel> lista = List();
-    for (var registro in dadosJson) {
-      UsuarioModel usuarioModel = UsuarioModel.fromJson(registro); //.converteJson
-      lista.add(usuarioModel);
-    }
-    return lista;
+    String url = "${ConstantesRest.URL_USUARIOS}/filter";
+    UsuarioRest usuarioRest = UsuarioRest();
+    return usuarioRest.processaHttpPostReturn(url, usuarioModel, teste);
   }
 
   Future<List<UsuarioModel>> listaPorFiltroComFlagAmigoDoUsuario(String idUsuario, var usuarioModel, {bool teste}) async {
-
-    http.Response response = await http.post(
-        "${ConstantesRest.URL_USUARIOS}/filterComFlagAmigoDoUsuario/${idUsuario}",
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: {
-          jsonDecode(usuarioModel)
-        }
-    );
-    var dadosJson = json.decode(response.body);
-
-    if (teste != null && teste == true) {
-      UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
-      dadosJson = serviceFixo.responseLista();
-    }
-    List<UsuarioModel> lista = List();
-    for (var registro in dadosJson) {
-      UsuarioModel usuarioModel = UsuarioModel.fromJson(registro); //.converteJson
-      lista.add(usuarioModel);
-    }
-    return lista;
+    String url = "${ConstantesRest.URL_USUARIOS}/filterComFlagAmigoDoUsuario/${idUsuario}";
+    UsuarioRest usuarioRest = UsuarioRest();
+    return usuarioRest.processaHttpPostReturn(url, usuarioModel, teste);
   }
 
   Future<List<UsuarioModel>> listaPorFiltroComFlagAmigo(var usuarioModel, {bool teste}) async {
-
-    http.Response response = await http.post(
-        "${ConstantesRest.URL_USUARIOS}/filterComFlagAmigo",
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: {
-          jsonDecode(usuarioModel)
-        }
-    );
-    var dadosJson = json.decode(response.body);
-
-    if (teste != null && teste == true) {
-      UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
-      dadosJson = serviceFixo.responseLista();
-    }
-    List<UsuarioModel> lista = List();
-    for (var registro in dadosJson) {
-      UsuarioModel usuarioModel = UsuarioModel.fromJson(registro); //.converteJson
-      lista.add(usuarioModel);
-    }
-    return lista;
+    String url = "${ConstantesRest.URL_USUARIOS}/filterComFlagAmigo";
+    UsuarioRest usuarioRest = UsuarioRest();
+    return usuarioRest.processaHttpPostReturn(url, usuarioModel, teste);
   }
 
   Future<List<UsuarioModel>> listaPorEstado(String estado, {bool teste}) async {
-
-    http.Response response = await http.get("${ConstantesRest.URL_USUARIOS}/filterEstado/${estado}");
-    if (response.statusCode == 200 || (teste != null && teste == true)) {
-      var dadosJson = json.decode(response.body);
-      if (teste != null && teste == true) {
-        UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
-        dadosJson = serviceFixo.responseLista();
-      }
-      List<UsuarioModel> lista = List();
-      for (var registro in dadosJson) {
-        UsuarioModel usuarioModel = UsuarioModel.fromJson(
-            registro); //.converteJson
-        lista.add(usuarioModel);
-      }
-      return lista;
-
-    }else{
-      throw Exception('Failed to load Tipo Usuario!!!');
-    }
+    String url = "${ConstantesRest.URL_USUARIOS}/filterEstado/${estado}";
+    UsuarioRest usuarioRest = UsuarioRest();
+    return usuarioRest.processaHttpGetList(url, teste);
   }
 
   Future<UsuarioModel> listaPorEmail(String email, {bool teste}) async {
-
-    http.Response response = await http.get("${ConstantesRest.URL_USUARIOS}/filterEmail/${email}");
-    if (response.statusCode == 200 || (teste != null && teste == true)) {
-      var dadosJson = json.decode(response.body);
-      if (teste != null && teste == true) {
-        UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
-        dadosJson = serviceFixo.responseObjeto();
-      }
-      return UsuarioModel.fromJson(dadosJson); //.converteJson
-
-    }else{
-      throw Exception('Failed to load Tipo Usuario!!!');
-    }
+    String url = "${ConstantesRest.URL_USUARIOS}/filterEmail/${email}";
+    UsuarioRest usuarioRest = UsuarioRest();
+    return usuarioRest.processaHttpGetObject(url, teste);
   }
 
   Future<List<UsuarioModel>> listaPorNome(String nome, {bool teste}) async {
-
-    http.Response response = await http.get("${ConstantesRest.URL_USUARIOS}/filterNome/${nome}");
-    if (response.statusCode == 200 || (teste != null && teste == true)) {
-      var dadosJson = json.decode(response.body);
-      if (teste != null && teste == true) {
-        UsuarioServiceFixo serviceFixo = UsuarioServiceFixo();
-        dadosJson = serviceFixo.responseLista();
-      }
-      List<UsuarioModel> lista = List();
-      for (var registro in dadosJson) {
-        UsuarioModel usuarioModel = UsuarioModel.fromJson(
-            registro); //.converteJson
-        lista.add(usuarioModel);
-      }
-      return lista;
-
-    }else{
-      throw Exception('Failed to load Tipo Usuario!!!');
-    }
+    String url = "${ConstantesRest.URL_USUARIOS}/filterNome/${nome}";
+    UsuarioRest usuarioRest = UsuarioRest();
+    return usuarioRest.processaHttpGetList(url, teste);
   }
 
 }
